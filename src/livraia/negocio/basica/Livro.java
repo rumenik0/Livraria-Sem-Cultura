84eeb6b8d86c2ded35559058d0aab793dea78965
aluno <aluno@ib-02.campus.local>
2017-11-16 21:18:40 -0300

    COM CLASSE BASICA - LIVRO

---
+++ src/livraia/negocio/basica/Livro.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package livraia.negocio.basica;
+
+/**
+ *
+ * @author aluno
+ */
+public class Livro {
+        private String titulo;
+        private String edicao;
+        private String ano;
+        private String editora;
+        private String genero;
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the edicao
+     */
+    public String getEdicao() {
+        return edicao;
+    }
+
+    /**
+     * @param edicao the edicao to set
+     */
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    /**
+     * @return the ano
+     */
+    public String getAno() {
+        return ano;
+    }
+
+    /**
+     * @param ano the ano to set
+     */
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    /**
+     * @return the editora
+     */
+    public String getEditora() {
+        return editora;
+    }
+
+    /**
+     * @param editora the editora to set
+     */
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+}

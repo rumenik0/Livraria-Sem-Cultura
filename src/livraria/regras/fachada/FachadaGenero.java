351e8d1443cebf7e174dfe3d4d8e2939bbee378b
aluno <aluno@ib-166.campus.local>
2017-11-18 15:12:02 -0300

    Thiago - FachadaGenero

---
+++ src/livraria/regras/fachada/FachadaGenero.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package livraria.regras.fachada;
+
+import livraia.negocio.basica.Genero;
+
+/**
+ *
+ * @author aluno
+ */
+public class FachadaGenero {
+    
+    public void cadastrarGenero(Genero g){
+    
+    }
+}

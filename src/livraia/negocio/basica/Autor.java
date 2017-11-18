2e4affb668bcb8409246d8507666a0c8eb0ced18
aluno <aluno@ib-309.campus.local>
2017-11-18 13:46:53 -0300

    Rumenik -Autor

---
+++ src/livraia/negocio/basica/Autor.java
@@ -0,0 +1,35 @@
+package livraia.negocio.basica;
+
+/**
+ *
+ * @author aluno
+ */
+public class Autor {
+    private Integer codigo;
+    private String nome;
+
+    /**
+     * @return the codigo
+     */
+    public Integer getCodigo() {
+        return codigo;
+    }
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

f5a19f5e8e21d1fd5cf39974b48272eb7e19f512
aluno <aluno@ib-165.campus.local>
2017-11-18 15:29:22 -0300

    Regra Autor e RegraFuncionario Nardelli

--- src/livraria/regras/fachada/FachadaCliente.java
+++ src/livraria/regras/fachada/FachadaCliente.java
@@ -6,14 +6,35 @@
 package livraria.regras.fachada;
 
 import livraia.negocio.basica.Cliente;
+import livraria.acesso.DAOClienteImpl;
+import livraria.erro.ConexaoException;
+import livraria.erro.DAOException;
+import livraria.erro.RegraException;
+import livraria.regras.RegraCliente;
 
 /**
  *
  * @author Pedro Nardelli
  */
 public class FachadaCliente {
-    public void cadastrarCliente(Cliente c){
+    public void cadastrar(Cliente c) throws RegraException, DAOException, ConexaoException{
+        RegraCliente rc = new RegraCliente();
+        rc.validarDados(c);
         
+        DAOClienteImpl dci = new DAOClienteImpl();
+        dci.inserir(c);
+    
+    }
+    public void alterar(Cliente c)throws RegraException{
+    
+    }
+    public void remover(String cpf)throws RegraException{
+    }
+    public Cliente consultar(String cpf) throws DAOException, ConexaoException{
+        Cliente c = new Cliente();
+        DAOClienteImpl dci = new DAOClienteImpl();
+        c = dci.consultar(cpf);
+        return c;
     }
     
 }

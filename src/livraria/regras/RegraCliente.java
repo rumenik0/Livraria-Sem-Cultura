eba12bca326c42bed2bfd3d4268eef7cc4d06e81
aluno <aluno@ib-235.campus.local>
2017-11-23 21:47:24 -0300

    FachadaAutor e FachadaCliente - alterações

--- src/livraria/regras/RegraCliente.java
+++ src/livraria/regras/RegraCliente.java
@@ -6,6 +6,9 @@
 package livraria.regras;
 
 import livraia.negocio.basica.Cliente;
+import livraria.acesso.DAOClienteImpl;
+import livraria.erro.ConexaoException;
+import livraria.erro.DAOException;
 import livraria.erro.RegraException;
 
 /**
@@ -13,28 +16,35 @@ import livraria.erro.RegraException;
  * @author Pedro Nardelli e Gandof
  */
 public class RegraCliente {
-    /**
-     * Validar a integridade dos dados do cliente.
-     * @param c
-     * @throws RegraException 
-     */
+    private final DAOClienteImpl dci;
+    
+    
     public void validarDados(Cliente c) throws RegraException{
-        
-        if(c == null){
+        if(c == null)
            throw new RegraException("Objeto Nulo"); 
+        if(c.getNome().isEmpty() || c.getNome() == null )
+           throw new RegraException("Nome em branco");
+        if(c.getCpf().isEmpty() || c.getCpf() == null)
+           throw new RegraException("CPF em branco");
+        if(c.getSexo().isEmpty() || c.getSexo() == null)
+           throw new RegraException("Sexo em branco");
+        if(c.getTelefone().isEmpty() || c.getTelefone() == null)
+           throw new RegraException("Telefone em branco");
+    }
+    public boolean existe(Cliente c) throws RegraException, DAOException{
+        try{
+        c = dci.consultar(c.getCpf());
+        if(c == null)
+            throw new RegraException("Cliente não existe, não pode ser alterado");
+        }catch(DAOException e){
+            throw e;
+        }catch(RegraException e){
+            throw e;
+        }catch(ConexaoException e){
+            throw e;
         }
         
-        if(c.getNome().isEmpty() || c.getNome() == null ){
-           throw new RegraException("Nome em branco");
-        }
-        if(c.getCpf().isEmpty() || c.getCpf() == null){
-           throw new RegraException("CPF em branco");
-        }
-        if(c.getSexo().isEmpty() || c.getSexo() == null){
-           throw new RegraException("Sexo em branco");
-        }
-        if(c.getTelefone().isEmpty() || c.getTelefone() == null){
-           throw new RegraException("Telefone em branco");
-        }
+        
     }
+    
 }

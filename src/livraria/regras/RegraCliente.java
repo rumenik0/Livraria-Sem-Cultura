4b8d93c6f742d04a81520d72a895ef0b66c534f7
Rumenik <rumenik0@gmail.com>
2017-11-25 18:36:51 -0300

    Rumenik, adicionando alteracao de cliente na regra

--- src/livraria/regras/RegraCliente.java
+++ src/livraria/regras/RegraCliente.java
@@ -5,6 +5,8 @@
  */
 package livraria.regras;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import livraia.negocio.basica.Cliente;
 import livraria.acesso.DAOClienteImpl;
 import livraria.erro.ConexaoException;
@@ -16,9 +18,35 @@ import livraria.erro.RegraException;
  * @author Pedro Nardelli e Gandof
  */
 public class RegraCliente {
-    private final DAOClienteImpl dci;
-    
-    
+    private final DAOClienteImpl dao;
+    public RegraCliente(){
+        dao = new DAOClienteImpl();
+    }
+    public void inserir(Cliente c) throws RegraException, DAOException, ConexaoException{
+        try{
+            if (existe(c))
+                throw new RegraException("Já existe um cliente com este CPF!");
+        }catch(RegraException e){
+            throw e;
+        }catch(DAOException e){
+            throw e;
+        }catch(ConexaoException e){
+            throw e;
+        }
+    }
+    public void alterar(Cliente c) throws RegraException, DAOException, ConexaoException{
+        try {
+            if (existe(c))
+                throw new RegraException("Não existe cliente para este CPF!");
+        } catch (RegraException e) {
+            throw e;
+        } catch (DAOException e) {
+            throw e;
+        } catch (ConexaoException e) {
+            throw e;
+        }
+        
+    }
     public void validarDados(Cliente c) throws RegraException{
         if(c == null)
            throw new RegraException("Objeto Nulo"); 
@@ -31,14 +59,13 @@ public class RegraCliente {
         if(c.getTelefone().isEmpty() || c.getTelefone() == null)
            throw new RegraException("Telefone em branco");
     }
-    public boolean existe(Cliente c) throws RegraException, DAOException{
+    public boolean existe(Cliente c) throws RegraException, DAOException, ConexaoException{
         try{
-        c = dci.consultar(c.getCpf());
-        if(c == null)
-            throw new RegraException("Cliente não existe, não pode ser alterado");
+            c = dao.consultar(c.getCpf());
+            if(c == null)
+                return false;
+            return true;
         }catch(DAOException e){
-            throw e;
-        }catch(RegraException e){
             throw e;
         }catch(ConexaoException e){
             throw e;
